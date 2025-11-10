7e3735f0419a36e21d283f4df57f4bd153f24bc3
ANIKET PATRA <110764399+aniketpatra96@users.noreply.github.com>
2025-11-10 13:53:21 +0530

    done

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 9090;
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.post("/profile/upload", async (req, res) => {
   try {
