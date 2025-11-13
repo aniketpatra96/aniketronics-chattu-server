09d0ec24ecde9cedc838b99df96fab995b297588
ANIKET PATRA <110764399+aniketpatra96@users.noreply.github.com>
2025-11-13 19:16:11 +0530

    added cron.js

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import cloudinary from "./lib/cloudinary.js";
+import job from "./lib/cron.js";
 const PORT = process.env.PORT || 9090;
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+job.start();
 
 app.post("/profile/upload", async (req, res) => {
   try {
